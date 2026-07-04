a57a8792bc78cd8031c4ae5be33307e7fb757776
Ashish Kumar Sahoo <Ashishsahoo1896@gmail.com>
2026-07-04 20:59:57 +0530

    Update dialog_auto_enroll.py

--- src/components/dialog_auto_enroll.py
+++ src/components/dialog_auto_enroll.py
@@ -1,4 +1,4 @@
-import streamlit as st
+'''import streamlit as st
 from src.database.db import enroll_student_to_subject
 from src.database.config import supabase
 
@@ -42,3 +42,4 @@ def auto_enroll_dialog(subject_code):
             time.sleep(2)
             st.rerun()
 
+'''
