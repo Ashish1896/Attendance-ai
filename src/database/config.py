d446e52cf5cd0cc6ac9391dfaa14eaa1efe800e0
Ashish Kumar Sahoo <Ashishsahoo1896@gmail.com>
2026-07-04 21:41:11 +0530

    both streamlit and render database run

--- src/database/config.py
+++ src/database/config.py
@@ -1,9 +1,12 @@
+import os
 import streamlit as st
-
-
 from supabase import create_client, Client
 
-supabase: Client = create_client(
-    st.secrets["SUPABASE_URL"],
-    st.secrets["SUPABASE_KEY"]
-)
+url = os.getenv("SUPABASE_URL")
+key = os.getenv("SUPABASE_KEY")
+
+if url is None or key is None:
+    url = st.secrets["SUPABASE_URL"]
+    key = st.secrets["SUPABASE_KEY"]
+
+supabase: Client = create_client(url, key)
